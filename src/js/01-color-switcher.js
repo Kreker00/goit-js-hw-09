2072c95147ce636845dc334d4a5062e11651348a
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-09-23 01:03:48 +0300

    push 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.setAttribute('disabled', false);
+  buttonStop.removeAttribute('disabled');
+});
+
+buttonStop.addEventListener('click', () => {
+  buttonStart.removeAttribute('disabled');
+  buttonStop.setAttribute('disabled', false);
+  clearInterval(timerId);
+});
